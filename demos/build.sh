60814e483efddded4a1f4fa057562924494558df
Keanu Daniel <keanu.daniel@e-mail.de>
2023-07-28 06:54:24 +0200

    add demos

--- demos/build.sh
+++ demos/build.sh
@@ -4,4 +4,5 @@ DEPS="raylib"
 CFLAGS="-Wextra -Wall -pedantic -std=c11 `pkg-config --cflags ${DEPS}`"
 LIBS="`pkg-config --libs ${DEPS}` -lm"
 
-clang $CFLAGS -o geolib main.c $LIBS
+clang $CFLAGS -o scaling scaling.c $LIBS
+clang $CFLAGS -o adding adding.c $LIBS
